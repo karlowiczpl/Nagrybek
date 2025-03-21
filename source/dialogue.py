7f6f7eea46056886d0bab4fbc5728801b8c3d89c
karlowiczpl <karol.kar1@outlook.com>
2025-03-21 20:55:20 +0100

    dialogue2

--- source/dialogue.py
+++ source/dialogue.py
@@ -1,6 +1,7 @@
 import pygame
 
 class Dialog:
+    _wait_counter: int
     def __init__(self, dialog_text, who, window):
         self._who = who
         self._words = dialog_text.split()  # Podziel tekst na słowa
@@ -14,28 +15,44 @@ class Dialog:
         self._speed = 100  
         self._wait = False
         self._wait_counter = 0
+        self._counter = 0
+        self._stop = False
 
     def update(self):
         now = pygame.time.get_ticks()
-        if not self._wait:
-            if self._index < len(self._words) and now - self._last_update > self._speed:
-                self._current_text += " " + self._words[self._index]  
-                self._index += 1
-                self._last_update = now  
+        if not self._stop:
+            if not self._wait and not self._stop:
+                if self._index < len(self._words) and now - self._last_update > self._speed:
+                    self._current_text += " " + self._words[self._index]  
+                    self._index += 1
+                    self._last_update = now  
+                if self._index == len(self._words):
+                    self._stop = True
+            else:
+                self._counter += 1
+
+                if self._counter > 40:
+                    self._wait = False
+                    self._counter = 0
+                    self._current_text = ""
+
+            if len(self._current_text) > 60:
+                self._wait = True
+                self._wait_counter = 0
+
+            return True
         else:
-            self._wait_counter = self._wait_counter + 1
-            print(f"waiting, {self._wait_counter}")
+            self._counter += 1
 
-            if self._wait_counter > 40:
-                self._wait = False
-                self._current_text = ""
+            if self._counter > 40:
+                return False
 
-        if len(self._current_text) > 60:
-            self._wait = True
-            self._wait_counter = 0
+            return True
+
+
 
     def draw(self):
-        self.update()
+        up = self.update()
 
         dialog_x = (self._win.get_width() - self.dialog_width) // 2
         dialog_y = self._win.get_height() - 200
@@ -46,4 +63,6 @@ class Dialog:
         text_surface = self.font.render(self._current_text, True, (255, 255, 255))
         text_rect = text_surface.get_rect(center=(self._win.get_width() // 2, dialog_y + self.dialog_height // 2))
         self._win.blit(text_surface, text_rect)
+
+        return up
 
--- source/start.py
+++ source/start.py
@@ -22,12 +22,14 @@ class Start:
         self._info = pygame.display.Info()
         self._player = player
         self._dialog = Dialog(sample_dialog, "jan", self._win)
+        self._active_dialog = True
 
     def draw(self):
         background = pygame.transform.scale(bg, (self._info.current_w,self._info.current_h))
         self._win.blit(background, (0,0))
         self._player.draw()
-        self._dialog.draw()
+        if self._active_dialog:
+            self._active_dialog = self._dialog.draw()
 
     def key(self, key):
         if key[pygame.K_SPACE]:
